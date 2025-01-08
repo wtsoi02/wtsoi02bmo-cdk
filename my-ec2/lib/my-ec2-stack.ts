568ce40d3282d0c0dc419f8c9c1e8172b6ca2540
wtsoi02 <william.w.tsoi@gmail.com>
2025-01-08 23:26:27 +0530

    add wtsoi01-ec2vpc-day3 dir

--- my-ec2/lib/my-ec2-stack.ts
+++ my-ec2/lib/my-ec2-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import { MachineImage } from 'aws-cdk-lib/aws-ec2';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class MyEc2Stack extends cdk.Stack {
+export class MyEc2Stackwtsoi01 extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const vpc = ec2.Vpc.fromLookup(this,'ExistingVPC', {
+        isDefault: true
+    });
+    const wtsoi01instance = new ec2.Instance(this, 'wtsoi01machine',{
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+      keyName: 'splunk-key',
+    });
+      
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
